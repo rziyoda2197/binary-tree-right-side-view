23f4d77b5c3863285bdebc942071d127b0b87393
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-20 15:54:50 +0500

    🚀 Add main application code

---
+++ src/index.js
@@ -0,0 +1,39 @@
+class Node {
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+function rightSideView(root) {
+    if (!root) return [];
+
+    let result = [];
+    let queue = [root];
+
+    while (queue.length > 0) {
+        let levelSize = queue.length;
+        for (let i = 0; i < levelSize; i++) {
+            let node = queue.shift();
+            if (i === levelSize - 1) result.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+    }
+
+    return result;
+}
+```
+
+Kodni ishlatish uchun misol:
+
+```javascript
+let root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.right = new Node(6);
+
+console.log(rightSideView(root)); // [1, 3, 6]
